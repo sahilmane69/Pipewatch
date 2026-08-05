773fa4625078a00122ef73fe9b7e3d04b475c69e
Sahil Mane <148791198+sahilmane69@users.noreply.github.com>
2026-08-05 22:01:58 +0530

    updated webhook.js

--- observer/src/routes/webhook.routes.js
+++ observer/src/routes/webhook.routes.js
@@ -4,11 +4,13 @@ const router = Router();
 
 router.post("/github", (req, res) => {
 
+    const event = req.headers["x-github-event"];
+
     console.log("=================================");
-    console.log("EVENT:", req.headers["x-github-event"]);
-    console.log("ACTION:", req.body.action);
-    console.log("REPOSITORY:", req.body.repository?.full_name);
-    console.log("BRANCH:", req.body.ref);
+    console.log("Event:", event);
+    console.log("Action:", req.body.action);
+    console.log("Repository:", req.body.repository?.full_name);
+    console.log("Branch:", req.body.ref);
     console.log("=================================");
 
     res.sendStatus(200);
