1beb478f359624e9b3e52150c04bd8884472cc9c
Sahil Mane <148791198+sahilmane69@users.noreply.github.com>
2026-08-05 23:41:18 +0530

    error fixed webhookjs

--- observer/src/routes/webhook.routes.js
+++ observer/src/routes/webhook.routes.js
@@ -1,15 +1,21 @@
+import { Router } from "express";
+import { parseWorkflowRun } from "../services/github.service.js";
+import { savePipelineRun } from "../services/pipeline.service.js";
+
+const router = Router();
+
 router.post("/github", async (req, res) => {
     const event = req.headers["x-github-event"];
 
     if (event === "workflow_run") {
         const data = parseWorkflowRun(req.body);
 
-        console.log("Parsed Data:", data);
+        await savePipelineRun(data);
 
-        const saved = await savePipelineRun(data);
-
-        console.log("Saved Row:", saved);
+        console.log("Saved:", data);
     }
 
     res.sendStatus(200);
 });
+
+export default router;
