648baa5f3074daaae7d3ed53745b3a728d497f46
Sahil Mane <148791198+sahilmane69@users.noreply.github.com>
2026-08-05 21:53:39 +0530

    webhook test 2

--- observer/src/routes/webhook.routes.js
+++ observer/src/routes/webhook.routes.js
@@ -3,10 +3,15 @@ import { Router } from "express";
 const router = Router();
 
 router.post("/github", (req, res) => {
-    console.log("Webhook Hit");
-    return res.status(200).json({
-        success: true
-    });
+
+    console.log("=================================");
+    console.log("EVENT:", req.headers["x-github-event"]);
+    console.log("ACTION:", req.body.action);
+    console.log("REPOSITORY:", req.body.repository?.full_name);
+    console.log("BRANCH:", req.body.ref);
+    console.log("=================================");
+
+    res.sendStatus(200);
 });
 
 export default router;
