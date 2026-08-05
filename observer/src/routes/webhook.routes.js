0f648cd8b27c1c9c22df7b814be992a9dd5847ac
Sahil Mane <148791198+sahilmane69@users.noreply.github.com>
2026-08-05 23:37:34 +0530

    webhook for debugging

--- observer/src/routes/webhook.routes.js
+++ observer/src/routes/webhook.routes.js
@@ -1,21 +1,15 @@
-import { Router } from "express";
-import { parseWorkflowRun } from "../services/github.service.js";
-import { savePipelineRun } from "../services/pipeline.service.js";
-
-const router = Router();
-
 router.post("/github", async (req, res) => {
     const event = req.headers["x-github-event"];
 
     if (event === "workflow_run") {
         const data = parseWorkflowRun(req.body);
 
-        await savePipelineRun(data);
+        console.log("Parsed Data:", data);
 
-        console.log("Saved to database:", data);
+        const saved = await savePipelineRun(data);
+
+        console.log("Saved Row:", saved);
     }
 
     res.sendStatus(200);
 });
-
-export default router;
